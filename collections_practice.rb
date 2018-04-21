d2e13ea62af16106d5378c185b98f40a5de476fa
Aaron Na <syn228@nyu.edu>
2018-04-21 22:59:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(array)
+  if array.all? {|word| word.start_with?("r")}
+  return true
+  else return false
+  end
+end
+
+def contain_a(array)
+  array.find_all {|word| word.include?("a")}
+end
+
+def first_wa(array)
+  array.find do |word|
+    string[0] == "w" && string[1] == "a"
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |element|
+  !element.is_a?(String)
+  end
+end
+
+def count_elements(array)
+   
+end
+
+def merge_data
+  
+end
+
+def find_cool
+  
+end
+
+def organize_schools
+  
+end
